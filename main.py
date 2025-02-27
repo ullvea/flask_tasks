c88d1501bf5dc9824cc9805a209c55f3a747c7b2
ullvea <sofimastikova@gmail.com>
2025-02-27 23:58:01 +0300

    реклама с картинкой

---
+++ main.py
@@ -0,0 +1,89 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index1():
+    return "Миссия Колонизация Марса!"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!!"
+
+
+@app.route('/promotion')
+def promotion():
+    countdown_list = ['Человечество вырастает из детства.',
+
+                      'Человечеству мала одна планета.',
+
+                      'Мы сделаем обитаемыми безжизненные пока планеты.',
+
+                      'И начнем с Марса!',
+
+                      'Присоединяйся!']
+    return '</br>'.join(countdown_list)
+
+
+@app.route('/')
+@app.route('/image_mars')
+def image_mars():
+    return '''<!doctype html>
+                   <html lang="en">
+                     <head>
+                       <meta charset="utf-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet" 
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                       crossorigin="anonymous">
+                       <title>Привет, Марс!</title>
+                     </head>
+                     <body>
+                       <h1>Жди нас, Марс!</h1>
+                       <img src="/static/image_mars/MARS.png"
+                       <p>
+                       <h6>Вот она какая, красная планета.</h6>
+                       </p>
+                     </body>
+                   </html>'''
+
+
+@app.route('/')
+@app.route('/promotion_image')
+def promotion_image():
+    return '''<!doctype html>
+                   <html lang="en">
+                     <head>
+                       <meta charset="utf-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet" 
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                       crossorigin="anonymous">
+                       <title>Колонизация</title>
+                     </head>
+                     <body>
+                       <link rel="stylesheet" href="static/css/style.css">
+                       <h1>Жди нас, Марс!</h1>
+                       <img src="/static/image_mars/MARS.png"
+                       <p>
+                       <div class="alert alert-secondary" role="alert">
+                       </br>Человечество вырастает из детства.</div>
+                       <div class="alert alert-success" role="alert">
+                       </br>Человечеству мала одна планета.</div>
+                       <div class="alert alert-secondary" role="alert">
+                       </br>Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                       <div class="alert alert-warning" role="alert">
+                       </br>И начнем с Марса!</div>
+                       <div class="alert alert-danger" role="alert">
+                       </br>Присоединяйся!</div>
+                       </p>
+                     </body>
+                   </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
