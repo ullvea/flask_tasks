d2e8fa36e69ab66c8e77f91c0eb8986aecfb0f1f
ullvea <sofimastikova@gmail.com>
2025-02-28 00:09:05 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -84,6 +84,36 @@ def promotion_image():
                      </body>
                    </html>'''
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f'''<!doctype html>
+                   <html lang="en">
+                     <head>
+                       <meta charset="utf-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet" 
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                       crossorigin="anonymous">
+                       <title>Варианты выбора</title>
+                     </head>
+                     <body>
+                       <h1>Мое предложение:{planet_name}</h1>
+                       <h2>Эта планета близка к Земле;</h2>
+                       <p>
+                       <h2>
+                       <div class="alert alert-success" role="alert">
+                       </br>На ней много необходимых ресурсов;</div>
+                       <div class="alert alert-secondary" role="alert">
+                       </br>На ней есть вода и атмосфера;</div>
+                       <div class="alert alert-warning" role="alert">
+                       </br>На ней есть небольшое магнитное поле;</div>
+                       <div class="alert alert-danger" role="alert">
+                       </br>Наконец, она просто красива!</div>
+                       </p>
+                     </body>
+                   </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
