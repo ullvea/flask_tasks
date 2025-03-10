54be1c0a357c182c57b0b300a42e429fa5b02685
ullvea <sofimastikova@gmail.com>
2025-03-10 22:16:24 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -144,5 +144,26 @@ def results(nickname, level, rating):
                    </html>'''
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                       <html lang="en">
+                         <head>
+                           <meta charset="utf-8">
+                           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                           <link rel="stylesheet" 
+                           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                           crossorigin="anonymous">
+                           <title>Пейзажи Mарса</title>
+                         </head>
+                         <body>
+                            <div align="center">
+                           <h1>Пейзажи Марса</h1>
+                           
+                         </body>
+                       </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
