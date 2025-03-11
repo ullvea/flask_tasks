84d880a69ec6a0e9ffc13eac8c9e98048e3be5e4
ullvea <sofimastikova@gmail.com>
2025-03-11 23:26:56 +0300

    Carousel

--- main.py
+++ main.py
@@ -159,9 +159,36 @@ def carousel():
                          <body>
                             <div align="center">
                            <h1>Пейзажи Марса</h1>
-                           
-                         </body>
-                       </html>'''
+                           <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+                              <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img src="static/landscapes/image1.jpg" class="d-block w-100" alt="image1">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="static/landscapes/image2.jpg" class="d-block w-100" alt="image2">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="static/landscapes/image3.jpg" class="d-block w-100" alt="image3">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+                            </body>
+                          </html>'''
 
 
 if __name__ == '__main__':
